4e16e900f3f8ac61d1e39d7d628bb7d8ada7a92f
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2019-03-04 18:20:31 +0000

    programmes list fixed

--- src/app/programmesList/programmesList.component.ts
+++ src/app/programmesList/programmesList.component.ts
@@ -61,22 +61,58 @@ export class ProgrammesListComponent implements OnInit {
       showLg: 'true'
     }
   ];
-
   programmesItemList: Array<ProgrammeItem> = [];
-
-  domainList = [
-    'All',
-    'Computer Science'
-  ];
-
+  programmesItemViewList: Array<ProgrammeItem> = [];
+  programmesItemView2List: Array<ProgrammeItem> = [];
+  pageSizeOptions: number[] = [5, 10, 15, 20];
+  domainList = [];
+  selectedDomain: string;
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.programmesItemList = this.uniItem.programmes;
+    this.domainList = ['All'];
+    for (const prog of this.programmesItemList) {
+      if (this.domainList.indexOf(prog.domain) === -1) {
+        this.domainList.push(prog.domain);
+      }
+    }
+    this.selectedDomain = this.domainList[0];
+    this.search();
+    this.updateView({
+      pageIndex: 0,
+      pageSize: 10
+    });
   }
 
   openProgramme(programmeItem: any) {
     window.location.href = programmeItem.url;
   }
 
+  search() {
+    console.log(this.selectedDomain);
+    this.programmesItemViewList = [];
+
+    for (const prog of this.programmesItemList) {
+      if (this.selectedDomain === prog.domain || this.selectedDomain === 'All') {
+        this.programmesItemViewList.push(prog);
+      }
+    }
+    this.updateView({
+      pageIndex: 0,
+      pageSize: 10
+    });
+  }
+
+  updateView(event: any) {
+    console.log(event);
+
+    this.programmesItemView2List = [];
+
+    for (let i = event.pageIndex * event.pageSize; i < (event.pageIndex + 1 ) * event.pageSize; i++) {
+      if (i < this.programmesItemViewList.length) {
+        this.programmesItemView2List.push(this.programmesItemViewList[i]);
+      }
+    }
+  }
 }
